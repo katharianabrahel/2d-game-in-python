35f1432db3cbda5632f4c7beff12c65a30ad04d6
katharianabrahel <katharian.abrahel@gmail.com>
2022-10-26 14:27:33 -0300

    adicionando animação do player e dos coletáveis

--- jogo.py
+++ jogo.py
@@ -25,17 +25,63 @@ def cronometro(tempo):
     return tempo_restante
 
 def andar(velocidade):
+    global player_status, player_direita
     keys = pygame.key.get_pressed()
     if velocidade:
         if keys[pygame.K_RIGHT]:
+            player_direita = True
+            player_status = 'Run'
             player_rect.x += 8
         elif keys[pygame.K_LEFT]:
+            player_direita = False
+            player_status = 'Run'
             player_rect.x -= 8
     else:
         if keys[pygame.K_RIGHT]:
+            player_status = 'Walk'
+            player_direita = True
             player_rect.x += 4
         elif keys[pygame.K_LEFT]:
+            player_status = 'Walk'
+            player_direita = False
             player_rect.x -= 4
+
+def animacao_coletavel():
+    global coletavel1, coletavel2, coletavel3, coin_index
+    coin_index += 0.1
+    if coin_index >= len(coin):
+        coin_index = 0
+    coletavel1 = coin[int(coin_index)]
+    coletavel2 = coin[int(coin_index)]
+    coletavel3 = coin[int(coin_index)]
+
+def animacao_player():
+    global player_index, player_direita, player_status, player_animacao, player
+    player_index += 0.3
+    if player_index >= 10:
+        player_index = 0
+    if player_status == 'Idle':
+        if player_direita:
+            player = player_animacao_idle[int(player_index)]
+        else:
+            player = pygame.transform.flip(player_animacao_idle[int(player_index)], True, False)
+    elif player_status == 'Walk':
+        if player_direita:
+            player = player_animacao_walk[int(player_index)]
+        else:
+            player = pygame.transform.flip(player_animacao_walk[int(player_index)], True, False)
+    elif player_status == 'Run':
+        if player_direita:
+            player = player_animacao_run[int(player_index)]
+        else:
+            player = pygame.transform.flip(player_animacao_run[int(player_index)], True, False)
+    elif player_status == 'Jump':
+        if player_direita:
+            player = player_animacao_jump[int(player_index)]
+        else:
+            player = pygame.transform.flip(player_animacao_jump[int(player_index)], True, False)
+
+
 
 pygame.init()
 tela = pygame.display.set_mode((1280, 720))
@@ -54,21 +100,79 @@ inimigo_teste.fill('yellow')
 inimigo_rect = inimigo_teste.get_rect(bottomleft = (1230, 600))
 movimento = 'esquerda'
 
-player_teste = pygame.Surface((50, 50))
-player_teste.fill('red')
-player_rect = player_teste.get_rect(midbottom = (100, 600))
+player_status = 'Idle'
+player_direita = True
+player_index = 0
+player_animacao_idle_1 = pygame.image.load('images/player/idle/Idle(1).png')
+player_animacao_idle_2 = pygame.image.load('images/player/idle/Idle(2).png')
+player_animacao_idle_3 = pygame.image.load('images/player/idle/Idle(3).png')
+player_animacao_idle_4 = pygame.image.load('images/player/idle/Idle(4).png')
+player_animacao_idle_5 = pygame.image.load('images/player/idle/Idle(5).png')
+player_animacao_idle_6 = pygame.image.load('images/player/idle/Idle(6).png')
+player_animacao_idle_7 = pygame.image.load('images/player/idle/Idle(7).png')
+player_animacao_idle_8 = pygame.image.load('images/player/idle/Idle(8).png')
+player_animacao_idle_9 = pygame.image.load('images/player/idle/Idle(9).png')
+player_animacao_idle_10 = pygame.image.load('images/player/idle/Idle(10).png')
+player_animacao_idle = [player_animacao_idle_1, player_animacao_idle_2, player_animacao_idle_3, player_animacao_idle_4, player_animacao_idle_5, player_animacao_idle_6, player_animacao_idle_7, player_animacao_idle_8, player_animacao_idle_9, player_animacao_idle_10]
+
+player_animacao_walk_1 = pygame.image.load('images/player/walk/Walk(1).png')
+player_animacao_walk_2 = pygame.image.load('images/player/walk/Walk(2).png')
+player_animacao_walk_3 = pygame.image.load('images/player/walk/Walk(3).png')
+player_animacao_walk_4 = pygame.image.load('images/player/walk/Walk(4).png')
+player_animacao_walk_5 = pygame.image.load('images/player/walk/Walk(5).png')
+player_animacao_walk_6 = pygame.image.load('images/player/walk/Walk(6).png')
+player_animacao_walk_7 = pygame.image.load('images/player/walk/Walk(7).png')
+player_animacao_walk_8 = pygame.image.load('images/player/walk/Walk(8).png')
+player_animacao_walk_9 = pygame.image.load('images/player/walk/Walk(9).png')
+player_animacao_walk_10 = pygame.image.load('images/player/walk/Walk(10).png')
+player_animacao_walk = [player_animacao_walk_1, player_animacao_walk_2, player_animacao_walk_3, player_animacao_walk_4, player_animacao_walk_5, player_animacao_walk_6, player_animacao_walk_7, player_animacao_walk_8, player_animacao_walk_9, player_animacao_walk_10]
+
+player_animacao_run_1 = pygame.image.load('images/player/run/Run(1).png')
+player_animacao_run_2 = pygame.image.load('images/player/run/Run(2).png')
+player_animacao_run_3 = pygame.image.load('images/player/run/Run(3).png')
+player_animacao_run_4 = pygame.image.load('images/player/run/Run(4).png')
+player_animacao_run_5 = pygame.image.load('images/player/run/Run(5).png')
+player_animacao_run_6 = pygame.image.load('images/player/run/Run(6).png')
+player_animacao_run_7 = pygame.image.load('images/player/run/Run(7).png')
+player_animacao_run_8 = pygame.image.load('images/player/run/Run(8).png')
+player_animacao_run_9 = pygame.image.load('images/player/run/Run(9).png')
+player_animacao_run_10 = pygame.image.load('images/player/run/Run(10).png')
+player_animacao_run = [player_animacao_run_1, player_animacao_run_2, player_animacao_run_3, player_animacao_run_4, player_animacao_run_5, player_animacao_run_6, player_animacao_run_7, player_animacao_run_8, player_animacao_run_9, player_animacao_run_10]
+
+player_animacao_jump_1 = pygame.image.load('images/player/jump/Jump(1).png')
+player_animacao_jump_2 = pygame.image.load('images/player/jump/Jump(2).png')
+player_animacao_jump_3 = pygame.image.load('images/player/jump/Jump(3).png')
+player_animacao_jump_4 = pygame.image.load('images/player/jump/Jump(4).png')
+player_animacao_jump_5 = pygame.image.load('images/player/jump/Jump(5).png')
+player_animacao_jump_6 = pygame.image.load('images/player/jump/Jump(6).png')
+player_animacao_jump_7 = pygame.image.load('images/player/jump/Jump(7).png')
+player_animacao_jump_8 = pygame.image.load('images/player/jump/Jump(8).png')
+player_animacao_jump_9 = pygame.image.load('images/player/jump/Jump(9).png')
+player_animacao_jump_10 = pygame.image.load('images/player/jump/Jump(10).png')
+player_animacao_jump = [player_animacao_jump_1, player_animacao_jump_2, player_animacao_jump_3, player_animacao_jump_4, player_animacao_jump_5, player_animacao_jump_6, player_animacao_jump_7, player_animacao_jump_8,player_animacao_jump_9, player_animacao_jump_10 ]
+
+player = player_animacao_idle[player_index]
+player_rect = player.get_rect(midbottom = (100, 607))
 player_gravity = 20
 
-coletavel1 = pygame.Surface((10, 10))
-coletavel1.fill('blue')
+coin1 = pygame.image.load('images/coin/coin_1.png')
+coin2 = pygame.image.load('images/coin/coin_2.png')
+coin3 = pygame.image.load('images/coin/coin_3.png')
+coin4 = pygame.image.load('images/coin/coin_4.png')
+coin5 = pygame.image.load('images/coin/coin_5.png')
+coin6 = pygame.image.load('images/coin/coin_6.png')
+coin7 = pygame.image.load('images/coin/coin_7.png')
+coin8 = pygame.image.load('images/coin/coin_8.png')
+coin = [coin1, coin2, coin3, coin4, coin5, coin6, coin7, coin8]
+coin_index = 0
+
+coletavel1 = coin[coin_index]
 coletavel1_rect = coletavel1.get_rect(topleft = (300, 400))
 
-coletavel2 = pygame.Surface((10, 10))
-coletavel2.fill('blue')
+coletavel2 = coin[coin_index]
 coletavel2_rect = coletavel1.get_rect(topleft = (600, 400))
 
-coletavel3 = pygame.Surface((10, 10))
-coletavel3.fill('blue')
+coletavel3 = coin[coin_index]
 coletavel3_rect = coletavel1.get_rect(topleft = (900, 400))
 
 energia_coletavel = pygame.Surface((10, 10))
@@ -112,7 +216,8 @@ while True:
         movimento = 'esquerda'
         inimigo_rect.x -= 2
 
-    tela.blit(player_teste, player_rect) 
+    animacao_player()
+    tela.blit(player, player_rect) 
     if player_rect.colliderect(energia_coletavel_rect):
         velocidade = True
         energia_coletavel_rect.x = -300
@@ -120,12 +225,15 @@ while True:
         andar(velocidade)
         if event.key == pygame.K_UP and player_rect.bottom >= 600:
             player_gravity = -20
+    else:
+        player_status = 'Idle'
 
-    if player_rect.x < 0:
-        player_rect.x = 0
-    elif player_rect.x > 1230:
-        player_rect.x = 1230
+    if player_rect.x < -20:
+        player_rect.x = -20
+    elif player_rect.x > 1200:
+        player_rect.x = 1200
 
+    animacao_coletavel()
     tela.blit(coletavel1, coletavel1_rect)
     tela.blit(coletavel2, coletavel2_rect)
     tela.blit(coletavel3, coletavel3_rect)
@@ -148,14 +256,16 @@ while True:
             player_gravity = -10
         else:
             vidas -= 1
-            player_rect = player_teste.get_rect(midbottom = (100, 600))
+            player_rect = player.get_rect(midbottom = (100, 600))
             inimigo_rect = inimigo_teste.get_rect(bottomleft = (1230, 600))
     if player_rect.colliderect(vida_coletavel_rect):
         if vidas < 3:
             vidas += 1
         vida_coletavel_rect.x = -500
-    if player_rect.bottom >= 600:
-        player_rect.bottom = 600
+    if player_rect.bottom >= 607:
+        player_rect.bottom = 607
+    elif player_rect.bottom < 607:
+        player_status = 'Jump'
 
     contador_vidas()
     pygame.display.update()
