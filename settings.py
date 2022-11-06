b6834103a8cdc918056e7568826b46b7b0f94c64
katharianabrahel <katharian.abrahel@gmail.com>
2022-11-06 20:38:52 -0300

    alteração no mapa

--- settings.py
+++ settings.py
@@ -1,14 +1,14 @@
 level_map = [
     'X                                                                         ',
-    'X                                                                         X',
+    'X         C                                                               X',
     'XC     W    EW         C                                                  X',
     'XXX     XXXXX          XX                                                 X',
     'XXXC                     W  H EW                              C           X',
     'XXXXX          XX         XXXXX      C    C    C             XXX          X',
-    'XXXXX        XX     B              W  EWW  EWW  EW   H      XXXXX      C  X',
-    'XXX     X  XXXXC   XX  XX       XX  XXX  XXX  XXX W   EW   XXXXXXX   WE W X',
-    'X       X  XXXX    XX  XXX  C  XXX  XXX  XXX  XXX  XXXX  XXXXXXXXXXX  XX  X',
-    'XP   XXXX  XXXXXX  XX  XXXXXXXXXXX  XXX  XXX  XXX  XXXX  XXXXXXXXXXX  XX  X',
+    'XXXXX        XXXX   B              W  EWW  EWW  EW   H      XXXXX      C  X',
+    'XXX     X  XXXXXX  XX  XX       XX  XXX  XXX  XXX W   EW   XXXXXXX   WE W X',
+    'X    P  X  XXXXXX  XX  XXX  C  XXX  XXX  XXX  XXX  XXXX  XXXXXXXXXXX  XX  X',
+    'X    XXXX  XXXXXX  XX  XXXXXXXXXXX  XXX  XXX  XXX  XXXX  XXXXXXXXXXX  XX  X',
     'XXXXXXXXX  XXXXXX  XX  XXXXXXXXXXX  XXX  XXX  XXX  XXXX  XXXXXXXXXXX  XX  X'
 ]
 
